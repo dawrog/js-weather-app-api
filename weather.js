a7a55d20c16a8d779657104db64c1eb1cd34a6fb
dawrog <dawid.rogal@protonmail.com>
2017-12-10 22:54:37 +0100

    added offline functionality JS

--- weather.js
+++ weather.js
@@ -1,36 +1,37 @@
-var loc;
 var temp;
+var loc;
+var icon;
 var humidity;
 var wind;
 var direction;
-var icon;
+
 
 function update(weather) {
-    $("#location").html(weather.humidity);
-    $("#temperature").html(weather.humidity);
-    $("#humidity").html(weather.humidity);
-    $("#wind").html(weather.humidity);
-    $("#direction").html(weather.humidity);
-    $("#icon").html(weather.humidity);
+    humidity.innerHTML = weather.humidity;
+    wind.innerHTML = weather.wind;
+    direction.innerHTML = weather.direction;
+    loc.innerHTML = weather.location;
+    temp.innerHTML = weather.temp;
+    icon.src = "imgs/codes/" + weather.icon + ".png"
+    console.log(icon.src);
 }
 
-$(document).ready(function() {
-    
-    location = $( "#location" );
-    temp = $( "#temperature" );
-    humidity = $( "#humidity" );
-    wind = $( "#wind" );
-    direction = $( "#direction" );
-    icon = $( "#icon" );
-});
+  window.onload = function () {
+  temp = document.getElementById("temperature");
+  loc = document.getElementById("location");
+  icon = document.getElementById("icon");
+  humidity = document.getElementById("humidity");
+  wind = document.getElementById("wind");
+  direction = document.getElementById("direction");
 
-var weather = {};
-weather.loc = "Sosnowiec"; 
-weather.temp = "0";
-weather.humidity = 20;
-weather.wind = 2.2;
-weather.direction = "W";
-weather.icon = 200;
-
-update(weather);
+  var weather = {};
+  weather.wind = 3.5;
+  weather.direction = "N";
+  weather.humidity = 35;
+  weather.location = "Sosnowiec";
+  weather.temp = "0";
+  weather.icon = "200";
+  
+  update(weather);
+} 
 
