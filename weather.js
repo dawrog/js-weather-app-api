a749e487ae51a6d30306fa9cdb94d6daa83444c0
dawrog <dawid.rogal@protonmail.com>
2017-12-12 23:27:33 +0100

    added weather zip update from api

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+var APPID = "d6c47c791927d46646402148554b908d";
 var temp;
 var loc;
 var icon;
@@ -5,6 +6,31 @@ var humidity;
 var wind;
 var direction;
 
+function updateByZip(zip) {
+  var url = "http://api.openweathermap.org/data/2.5/weather?" + "zip=" + zip + 
+  "&APPID=" + APPID;
+sendRequest(url);
+}
+
+function sendRequest(url) {
+  var xmlhttp = new XMLHttpRequest ();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+       var data = JSON.parse(xmlhttp.responseText);
+       var  weather = {};
+       weather.icon = data.weather[0].id;
+       weather.humidity = data.main.humidity;
+       weather.wind = data.wind.speed;
+       weather.direction = data.wind.deg;
+       weather.loc = data.name;
+       weather.temp = data.main.temp;
+       update(weather);
+  
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
 
 function update(weather) {
     humidity.innerHTML = weather.humidity;
@@ -24,7 +50,9 @@ function update(weather) {
   wind = document.getElementById("wind");
   direction = document.getElementById("direction");
 
-  var weather = {};
+  updateByZip(99501);
+
+  /*var weather = {};
   weather.wind = 3.5;
   weather.direction = "N";
   weather.humidity = 35;
@@ -32,6 +60,6 @@ function update(weather) {
   weather.temp = "0";
   weather.icon = "200";
   
-  update(weather);
+  update(weather); */
 } 
 
