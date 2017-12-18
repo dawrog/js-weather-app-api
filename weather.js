7febabd72a439f8b351e5cf557c23da9571fc6a1
dawrog <dawid.rogal@protonmail.com>
2017-12-18 23:32:57 +0100

    fixed geolocation bug in js

--- weather.js
+++ weather.js
@@ -15,8 +15,8 @@ sendRequest(url);
 function updateByGeo(lat, lon) {
     var url = "http://api.openweathermap.org/data/2.5/weather?" + 
         "lat=" + lat +
-        "&lon" + lon +
-        "&APPID" + APPID;
+        "&lon=" + lon +
+        "&APPID=" + APPID;
     sendRequest(url);
 }
 
